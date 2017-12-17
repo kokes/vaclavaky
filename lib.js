c81b4a6e56b6bf3cf7ffc61acf69f5cede933945
Ondrej Kokes <ondrej.kokes@gmail.com>
2017-12-17 20:26:02 +0100

    html -> html+js

---
+++ lib.js
@@ -0,0 +1,143 @@
+
+let unitConv = {
+    // normalize: m2
+    'm2': 1,
+    'km2': 1000 * 1000,
+    'ha': 100 * 100,
+    'msq': 1609.344 * 1609.344,
+
+    // normalize: kc
+    'kc': 1,
+    'usd': 21.475, // 1/12/2017
+    'eur': 25.525,
+    'gbp': 28.967,
+
+    // normalize: kg
+    'kg': 1,
+    'tn': 1000,
+    'lb': 0.453592,
+
+    // normalize: people
+    '1': 1,
+    '2': 2,
+    '4': 4,
+
+    //normalize: m
+    'm': 1,
+    'km': 1000
+};
+
+let convTg = {
+    // kg
+    'Váha': [
+        [ 1150, 'vozů Škoda Fabia'],
+        [ 439985, 'Boeingů 747-8'],
+        [ 1/6, 'Kč ve víčkách'],
+    ],
+    // m^2
+    'Rozloha': [
+        [ 42255, 'Václaváků'], // wiki říká 682 * 60, ale Cíba kouká do katastru
+        // Karlák?
+        [ 11.626 * 1000 * 1000, 'nádrží Slapy'],
+        [ 105 * 68, 'fotbalových hřišť v Edenu'],
+        [ 310.5 * 202.5, 'Strahovských stadionů'],
+        [ 496 * 1000 * 1000, 'rozloh Prahy'],
+        [ 696241 * 1000 * 1000, 'rozloh Texasu'],
+        [ 78866 * 1000 * 1000, 'rozloh ČR'],
+    ],
+    // kč
+    'Cena': [
+        [ 29, 'cheeseburgerů'],
+        [ 100, 'fidget spinnerů'],
+        [ 1309.3 * 1000 * 1000 * 1000, 'ročních rozpočtů Česka'], // 2017
+        [ 88 * 1000 * 1000 * 1000, 'majetků Andreje Babiše'],
+        // tuzemaku, vicek, platu poslance, iphonu, casopisu vlasta
+        [ 29346, 'průměrných mezd'],
+        [ 37046, 'průměrných mezd v Praze'],
+        [ 150, 'lahví tuzemáku'],
+        [ 6, 'kilogramů víček'], // víčka - 6 Kč/kg
+        [ 243900, 'vozů Škoda Fabia'], // http://www.skoda-auto.cz/modely/fabia/fabia
+    ],
+    'Osob': [
+        [ 11, 'Klapzubových jedenáctek'],
+        [ 90000, 'naplněných Wembley'],
+        [ 250000, 'naplněných Strahovských stadionů'],
+        [ 17360, 'naplněných pražských O2 aren'],
+        [ 10578820, 'Českých republik'],
+        [ 868, 'plných Airbusů A380-800'],
+    ],
+    // m
+    'Vzdálenost': [
+        [ 384400 * 1000, 'krát ze Země na Měsíc'],
+        [ 149597870700, 'krát ze Země na Slunce (AU)'],
+        [ 205 * 1000, 'krát z Prahy do Brna'],
+        [ 24.5, 'vagónů dlouhý vlak'],
+        [ 3.992, 'vozů Škoda Fabia za sebou'],
+        [ 40075 * 1000, 'krát okolo Země (na rovníku)'],
+        [ 6371 * 1000, 'poloměrů Země'],
+    ]
+};
+
+let units = {
+    'Váha': 'kg',
+    'Rozloha': 'm2',
+    'Cena': 'Kč',
+    'Osob': 'lidí',
+    'Vzdálenost': 'm',
+}
+
+// functions
+
+function parseInput(inp) {
+    let ret = inp.trim().replace(/\s+/g, '');
+    ret = ret.replace(',', '.');
+
+    return parseFloat(ret);
+}
+
+function numFormat(val) {
+    let ret = val.toFixed(2).replace('.', ',');
+    ret = ret.replace(/,0+$/, '');
+    if (ret === '0') {
+        return '< 0,01';
+    }
+    return ret;
+}
+
+function natVal(val) {
+    let nat = [
+        [12, 'bilionů'],
+        [9, 'miliard'],
+        [6, 'milionů'],
+        [3, 'tisíc']
+    ]
+    for (let el of nat) {
+        if (val >= Math.pow(10, el[0])) {
+            let nval = val / Math.pow(10, el[0]);
+            return `${numFormat(nval)} ${el[1]}`;
+        }
+    }
+    return numFormat(val);
+}
+
+function numToText(number, mul, unit, gr) {
+    let num = parseInput(number);
+    if (isNaN(num)) {
+        return null;
+    }
+
+    num *= Math.pow(10, parseInt(mul));
+
+    num *= unitConv[unit];
+
+    let res = [];
+    for (let el of convTg[gr]) {
+        let nval = natVal(num / el[0]);
+        res.push({
+            value: nval,
+            unit: el[1],
+        });
+    }
+
+    return res;
+}
